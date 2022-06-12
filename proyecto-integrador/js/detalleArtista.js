8e2a9980b2383dfb1eb4d9d104e3fda3f6b84db4
Trombola <trombola@udesa.edu.ar>
2022-06-12 10:52:24 -0300

    detalle artista

--- proyecto-integrador/js/detalleArtista.js
+++ proyecto-integrador/js/detalleArtista.js
@@ -1,45 +1,45 @@
-let sec = document.querySelector('.dArtista')
-let guardarDatos = ''
+let sec = document.querySelector('.dArtista');
+let guardarDatos = '';
 let qs = location.search;
 let qsObj = new URLSearchParams(qs);
-let captura = qsObj.get('id')
+let captura = qsObj.get('id');
 
-
+// obtenemos la informacion del cantante clickeado en la home
 fetch(`https://api.allorigins.win/raw?url= https://api.deezer.com/artist/${captura}`)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    fetch(`https://api.allorigins.win/raw?url= ${data.tracklist}`)
-.then(function (response) {
-    return response.json();
-})
-.then(function (data2) {
-    for (let i = 0; i < 5; i++) {
-        guardarDatos += `<li>${data2.data[i].title}</li>`
-    }
-    
-    console.log(guardarDatos);
-    
-    sec.innerHTML = `
-    <h1 class="titulo">${data.name}</h1>
-    <article class="imagen">
-    <img class='imagen' src="${data.picture}" alt="">
-    </article>
-    <article class="top5">
-    <h3>TOP 5 CANCIONES:</h3>
-    <ul class='song'>
-        ${guardarDatos}
-    </ul>
-    </article>`
-})
-.catch(function(errores){
-    console.log(errores);
-})
-    
-    
-    
-})
-.catch(function(errores){
-    console.log(errores);
-})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        fetch(`https://api.allorigins.win/raw?url= ${data.tracklist}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data2) {
+                for (let i = 0; i < 5; i++) {
+                    guardarDatos += `<li>${data2.data[i].title}</li>`
+                }
+
+                console.log(guardarDatos);
+
+                sec.innerHTML = `
+            <article class='art'>    
+                <h1>${data.name}</h1>
+                <img src="${data.picture}" alt="">
+            </article>
+            <article class='art'>
+                <h3>TOP 5 CANCIONES:</h3>
+                <ul>
+                    ${guardarDatos}
+                </ul>
+            </article>`
+            })
+            .catch(function (errores) {
+                console.log(errores);
+            })
+
+
+
+    })
+    .catch(function (errores) {
+        console.log(errores);
+    })
--- proyecto-integrador/js/detalleGenero.js
+++ proyecto-integrador/js/detalleGenero.js
@@ -1,28 +1,33 @@
 let locationQs = location.search
 let workQs = new URLSearchParams(locationQs)
 let capturarID = workQs.get('id')
-let array = ""
+console.log(capturarID);
+let secGenero = document.querySelector('.detalleGeneros')
 
-
-const url = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre${capturarID}`
-
-fetch(url)
+const url = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre` // de aca sacas el genero
+const url2 = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre/${capturarID}/artists` // de aca sacas el artista
+fetch(url) // traes todos los generos
 .then(function(response) {
   return response.json()
 })
 .then(function(res) {
 
-
-  let idDg = res.data
-  console.log(idDg);
-
-  
-  let dg = document.querySelector('.detalleGeneros')
-  for (let i = 0; i < array.length; i++) {
-    dg.innerHTML += `<p class="artistasDetalleGenero">${idDg[i].artists}</p>`
-
-    
+  console.log(res); // este objeto contiene los generos y su info
+fetch(url2) // traes los artistas del genero seleccionado
+.then(function(response) {
+  return response.json()
+})
+.then(function(res1) {
+  console.log(res1);
+  for (let i = 0; i < res.data.length; i++) {
+    if (capturarID==res.data[i].id)
+    console.log(res.data[i].name);
   }
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
+ 
 })
 .catch(function(error) {
   console.log("Error: " + error);
