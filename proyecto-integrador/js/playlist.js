42c515e78e1d0b10d86945bb9eaab3e8de9b7e33
antoniovillanuevau <avillanuevaugarte@udesa.edu.ar>
2022-06-18 19:34:15 -0300

    playlist terminado 100%

--- proyecto-integrador/js/playlist.js
+++ proyecto-integrador/js/playlist.js
@@ -4,20 +4,32 @@ let canciones = JSON.parse(recuperoStorage)
 
 if (canciones.length == 0) {
     section.innerHTML = '<p>no hay canciones en la playlist</p>'
-} else{
+} else {
     for (let i = 0; i < canciones.length; i++) {
         fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/${canciones[i]}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            section.innerHTML += `<h1>${data.title}</h1>`
-        })
-        .catch(function (errores) {
-            console.log(errores);
-        })
-        
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                section.innerHTML += `<article>
+            <h1>${data.title} </h1>  
+          
+            <ul>
+            <li>ARTISTA:</li>
+            <a href="./detalleArtista.html?id=${data.artist.id}"><li>${data.artist.name}</li></a>
+          </ul>
+         <img src="${data.album.cover}" alt="">
+            
+           
+
+         </article>`
+
+            })
+            .catch(function (errores) {
+                console.log(errores);
+            })
+
     }
 }
 
@@ -26,12 +38,12 @@ if (canciones.length == 0) {
 let formulario = document.querySelector('form.header')
 let campo = document.querySelector('.campo')
 formulario.addEventListener('submit', function (e) {
- e.preventDefault()
- if(campo.value == ''){
-    alert('el campo esta vacio')
- } else if (campo.value.length<3){
-    alert('el termino buscado debe tener al menos tres caracteres')
- }else{
-    this.submit()
- }
+    e.preventDefault()
+    if (campo.value == '') {
+        alert('el campo esta vacio')
+    } else if (campo.value.length < 3) {
+        alert('el termino buscado debe tener al menos tres caracteres')
+    } else {
+        this.submit()
+    }
 })
